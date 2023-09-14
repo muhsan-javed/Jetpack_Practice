fb74f3409d7f8c2415b7fceaf8409522fcc7994c
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-09-14 09:53:17 +0500

    State Ful Practice

--- app/src/main/java/com/muhsanjaved/jetpack_introduction/MainActivity.kt
+++ app/src/main/java/com/muhsanjaved/jetpack_introduction/MainActivity.kt
@@ -38,11 +38,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -93,6 +98,9 @@ class MainActivity : ComponentActivity() {
                     ImageAsset()
                     ImageNetwork()
                     IconsDemo()
+                    CartExample()
+                    StateFulExample()
+                    HelloScreen()
                 }
             }
         }
@@ -398,6 +406,36 @@ fun CartExample() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StateFulExample(){
+    var name : String by remember { mutableStateOf("Name") }
+
+    Column {
+        OutlinedTextField(value = name, onValueChange = {name = it})
+        Text(text = name, fontSize = 30.sp, fontFamily = myFontFamily)
+    }
+}
+
+// State Hosting
+@Composable
+fun HelloScreen(){
+    var name :String by remember { mutableStateOf("") }
+    HelloContent(name = name, onNameChange = {name = it})
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelloContent(name: String, onNameChange:(String) -> Unit){
+//    var name : String by remember { mutableStateOf("Name") }
+
+    Column {
+//        OutlinedTextField(value = name, onValueChange = {name = it})
+        OutlinedTextField(value = name, onValueChange = onNameChange)
+        Text(text = name, fontSize = 30.sp, fontFamily = myFontFamily)
+    }
+}
 
 
 @Composable
